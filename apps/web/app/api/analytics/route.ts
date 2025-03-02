726969694a5812d899316b952cf30fc246e35512
Keagan Ladds <ladds.k@gmail.com>
2025-03-02 09:07:05 +0100

    Testing to see whether using cache for the script file affects load time

--- apps/web/app/api/analytics/route.ts
+++ apps/web/app/api/analytics/route.ts
@@ -1,6 +1,17 @@
 import { NextResponse } from 'next/server';
 import fs from 'fs';
 import path from 'path';
+import { unstable_cache } from 'next/cache';
+
+const getScript = unstable_cache(
+  async () => {
+    // Read the minified analytics script
+    const scriptPath = path.join(process.cwd(), 'public', 'js', 'featherstats.min.js');
+    return await fs.promises.readFile(scriptPath, 'utf-8');
+  },
+  ['script'],
+  { revalidate: 36000 }
+)
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
@@ -10,9 +21,7 @@ export async function GET(request: Request) {
     return new NextResponse('API key is required', { status: 400 });
   }
 
-  // Read the minified analytics script
-  const scriptPath = path.join(process.cwd(), 'public', 'js', 'featherstats.min.js');
-  const scriptContent = fs.readFileSync(scriptPath, 'utf-8');
+  const scriptContent = await getScript();
 
   // Inject the API key into the script
   const scriptWithKey = `window.FEATHERSTATS_API_KEY = "${apiKey}";${scriptContent}`;
