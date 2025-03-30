09c09cc63b94a6a9b07973d1402f3ac7c96f67b2
Keagan Ladds <ladds.k@gmail.com>
2025-03-30 14:44:16 +0200

    Added missing environment var to turbo.json

--- apps/web/lib/stripe/server.ts
+++ apps/web/lib/stripe/server.ts
@@ -1,6 +1,6 @@
 import Stripe from "stripe"
 
 const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY;
-if (!STRIPE_SECRET_KEY) throw new Error();
+if (!STRIPE_SECRET_KEY) throw new Error("STRIPE_SECRET_KEY has not been defined");
 
 export const stripe = new Stripe(STRIPE_SECRET_KEY) ;
