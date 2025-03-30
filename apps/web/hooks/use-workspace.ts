2d7adda55988b3cb06feec85682ee2f0657a304b
Keagan Ladds <ladds.k@gmail.com>
2025-03-30 18:07:15 +0200

    Work on domain switcher and domain add dialog

--- apps/web/hooks/use-workspace.ts
+++ apps/web/hooks/use-workspace.ts
@@ -1,13 +1,33 @@
 import { WorkspaceContext } from "contexts/workspace-context";
-import { useContext } from "react";
-
+import { createDomain } from "lib/client/api-client";
+import { useCallback, useContext, useState } from "react";
+import { DomainCreateOptions } from "types/workspace";
+import { toast } from "sonner"
 export function useWorkspace() {
     const context = useContext(WorkspaceContext);
     if (!context) throw new Error("The 'useContext' hook can only be used within a WorkspaceProvider.")
 
-    const {domains} = context;
+    const [loading, setLoading] = useState<boolean>(false);
+    const { domains } = context;
+
+    const addDomain = useCallback(async (opts: DomainCreateOptions) => {
+        setLoading(true)
+        try {
+            const domain = await createDomain(context.workspace!.id, opts)
+            context.domains.push(domain)
+        } catch (err) {
+            toast.error("Something went wrong while creating domain, please refresh the page and try again.")
+            throw err;
+        } finally {
+            setLoading(false);
+        }
+
+    }, [])
 
     return {
-        domains
+        domains,
+        loading,
+        addDomain,
+        
     }
 }
