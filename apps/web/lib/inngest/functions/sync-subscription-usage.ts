7c890e268552eddcb03aa1f3405662aada8414f9
Keagan Ladds <ladds.k@gmail.com>
2025-04-09 15:32:29 +0200

    Added usage sync cronjob

---
+++ apps/web/lib/inngest/functions/sync-subscription-usage.ts
@@ -0,0 +1,30 @@
+import { subscriptionService } from "services/subscription.service";
+import { inngest } from "../client";
+import { usageService } from "services/usage.service";
+
+export const syncSubscriptionUsage = inngest.createFunction(
+    { id: "sync-subscription-usage" },
+    { event: "usage/sync" },
+    async ({ event, step }) => {
+        const subscriptionId = event.data.subscriptionId;
+        return usageService.syncSubscriptionUsage(subscriptionId)
+    });
+
+export const syncAllSubscriptionUsage = inngest.createFunction(
+    { id: "sync-all-subscription-usage" },
+    { cron: "*/30 * * * *" },
+    async ({ event, step }) => {
+
+        const subscriptionIds = await step.run("get-active-subscriptions", async () => {
+            return subscriptionService.getActiveSubscriptionIds()
+        });
+
+        for (var subscriptionId of subscriptionIds) {
+            await step.invoke(`sync-subscription-usage-${subscriptionId}`, {
+                function: syncSubscriptionUsage,
+                data: {
+                    subscriptionId: subscriptionId
+                }
+            });
+        }
+    });
