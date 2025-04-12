d68b1bcb3a45024bee829d3f3a60ab7c4f4bff87
Keagan Ladds <ladds.k@gmail.com>
2025-04-12 12:39:05 +0200

    Explicitly set API version on stripe to indicate new versions are not yet supported

--- apps/web/lib/stripe/server.ts
+++ apps/web/lib/stripe/server.ts
@@ -3,4 +3,6 @@ import Stripe from "stripe"
 const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY;
 if (!STRIPE_SECRET_KEY) throw new Error("STRIPE_SECRET_KEY has not been defined");
 
-export const stripe = new Stripe(STRIPE_SECRET_KEY) ;
+export const stripe = new Stripe(STRIPE_SECRET_KEY, {
+    apiVersion: '2025-02-24.acacia'
+}) ;
